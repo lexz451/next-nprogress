f60466d2708d57847d169a80890be4dcdc789531
Alejandro R. Zaldivar <lexz.zaldivar@gmail.com>
2023-10-11 18:17:50 -0700

    fix: add suspense to link component

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,20 +1,33 @@
 'use client';
 import { LinkProps as NextLinkProps } from 'next/link';
 import NextLink from 'next/link';
-import React from 'react';
+import React, { Suspense } from 'react';
 import { shouldTriggerProgressStart } from '../utils/shouldTriggerEventStart';
 import nProgress from 'nprogress';
 
-export default function Link(
-    props: NextLinkProps & { children: React.ReactNode } & React.RefAttributes<HTMLAnchorElement> & React.AnchorHTMLAttributes<HTMLAnchorElement>) {
+type LinkProps = NextLinkProps & { children: React.ReactNode } & React.RefAttributes<HTMLAnchorElement> & React.AnchorHTMLAttributes<HTMLAnchorElement>;
+
+function LinkFallback(props: LinkProps) {
     return (
-        <NextLink {...props} onClick={
-            (event) => {
-                if (shouldTriggerProgressStart(props.href, event)) nProgress.start();
-                if (props.onClick) props.onClick(event);
-            }
-        } className={props.className}>
+        <NextLink {...props}>
             {props.children}
         </NextLink>
+    )
+}
+
+export default function Link(props: LinkProps) {
+    return (
+        <Suspense fallback={
+            <LinkFallback {...props} />
+        }>
+            <NextLink {...props} onClick={
+                (event) => {
+                    if (shouldTriggerProgressStart(props.href, event)) nProgress.start();
+                    if (props.onClick) props.onClick(event);
+                }
+            }>
+                {props.children}
+            </NextLink>
+        </Suspense>
     )
 };
--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import React from "react";
 import { usePathname, useSearchParams } from "next/navigation";
 import nProgress, { NProgressOptions } from "nprogress";
